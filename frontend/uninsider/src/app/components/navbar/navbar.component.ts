aef1955161e3eab9f2a1e2121f437b9d819586f0
Iulian <iulian27_marius@yahoo.com>
2023-04-22 15:41:21 +0300

    resolved the reloading issue

--- frontend/uninsider/src/app/components/navbar/navbar.component.ts
+++ frontend/uninsider/src/app/components/navbar/navbar.component.ts
@@ -7,9 +7,20 @@ import { LoginService } from 'src/app/services/login.service';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+
+  isLoggedIn = false;
+  user:any = null;
+
   constructor(public login: LoginService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.isLoggedIn = this.login.isLoggedIn();
+    this.user = this.login.getUser();
+    this.login.loginStatusSubject.asObservable().subscribe(data => {
+      this.isLoggedIn = this.login.isLoggedIn();
+      this.user = this.login.getUser();
+    });
+  }
 
   public logout() {
     this.login.logout();
--- frontend/uninsider/src/app/services/login.service.ts
+++ frontend/uninsider/src/app/services/login.service.ts
@@ -1,11 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 import baseUrl from './helper';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+
+  public loginStatusSubject = new Subject<boolean>();
+  
   constructor(private http: HttpClient) { }
 
   // Get current user which is logged in
