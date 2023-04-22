59835eff89ee9a74b27a7ef2bfcb9253ffd91ea0
Iulian <iulian27_marius@yahoo.com>
2023-04-22 12:09:04 +0300

    set `isCredentialsNonExpired` to true

--- backend/uninsider/src/main/java/com/example/uninsider/model/User.java
+++ backend/uninsider/src/main/java/com/example/uninsider/model/User.java
@@ -82,7 +82,7 @@ public class User implements UserDetails {
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     public String getPassword() {
