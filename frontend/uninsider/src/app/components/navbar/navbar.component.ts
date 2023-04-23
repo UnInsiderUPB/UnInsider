dc78c8d0ba6fac998060c45597f7a40c242a28e0
Andrei-Bogdan Ciobanu <cb.andreibogdan@gmail.com>
2023-04-23 12:33:22 +0300

    style: change the style of the buttons of the navbar

--- frontend/uninsider/src/app/components/navbar/navbar.component.ts
+++ frontend/uninsider/src/app/components/navbar/navbar.component.ts
@@ -1,23 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from 'src/app/services/login.service';
-import {Router} from "@angular/router";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-
   isLoggedIn = false;
-  user:any = null;
+  user: any = null;
 
   constructor(public login: LoginService, public router: Router) {}
 
   ngOnInit(): void {
     this.isLoggedIn = this.login.isLoggedIn();
     this.user = this.login.getUser();
-    this.login.loginStatusSubject.asObservable().subscribe(data => {
+    this.login.loginStatusSubject.asObservable().subscribe((data) => {
       this.isLoggedIn = this.login.isLoggedIn();
       this.user = this.login.getUser();
     });
@@ -30,9 +29,8 @@ export class NavbarComponent implements OnInit {
 
   public toDashboard() {
     let user_role = this.login.getUserRole();
-    if (user_role == "ADMIN")
-      this.router.navigate(['/admin']).then(_ => {});
-    else if (user_role == "NORMAL")
-      this.router.navigate(['/user-dashboard']).then(_ => {});
+    if (user_role == 'ADMIN') this.router.navigate(['/admin']).then((_) => {});
+    else if (user_role == 'NORMAL')
+      this.router.navigate(['/user-dashboard']).then((_) => {});
   }
 }
