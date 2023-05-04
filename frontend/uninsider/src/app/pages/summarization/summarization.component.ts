630681d744b4cfd655fb0e93ff51954b22bde28a
Iulian <iulian27_marius@yahoo.com>
2023-05-04 18:57:21 +0300

    done basic summarization framework using HF

---
+++ frontend/uninsider/src/app/pages/summarization/summarization.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { HfInference } from '@huggingface/inference';
+
+const hf = new HfInference('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');
+
+@Component({
+  selector: 'app-summarization',
+  templateUrl: './summarization.component.html',
+  styleUrls: ['./summarization.component.css']
+})
+export class SummarizationComponent {
+
+  inputText: string = '';
+  summarizedText: string = '';
+  isSummarizationReady: boolean = false;
+
+  constructor() {}
+
+  public async summarize(): Promise<any> {
+    // Reset some variables
+    this.isSummarizationReady = false;
+    this.summarizedText = '';
+
+    let result = null;
+    try {
+      result = await hf.summarization({
+        model: 'facebook/bart-large-cnn',
+        inputs: this.inputText,
+      });
+    } catch (e) {
+      console.log(e);
+      alert('Error occurred while summarizing the text.');
+      return;
+    }
+
+    // Successfully summarized the text
+    console.log(result['summary_text']);
+    this.summarizedText = result['summary_text'];
+    this.isSummarizationReady = true;
+  }
+}
