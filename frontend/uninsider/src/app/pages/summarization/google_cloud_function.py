599c4c05c1740d7190457732affd09656fe01e39
Iulian <iulian27_marius@yahoo.com>
2023-05-06 12:43:24 +0300

    added functionality for initSummarizationModule

--- frontend/uninsider/src/app/pages/summarization/google_cloud_function.py
+++ frontend/uninsider/src/app/pages/summarization/google_cloud_function.py
@@ -1,5 +1,7 @@
-import transformers
-import functions_framework
+# `requirements.txt`
+# transformers==4.27.4
+# torch==2.0.0
+# functions-framework==3.3.0
 
 """
 This function is hosted on Google Cloud Functions.
@@ -11,17 +13,39 @@ and it accepts English text as input.
 
 Reference for enabling CORS requests [2].
 [2]: https://cloud.google.com/functions/docs/samples/functions-http-cors#functions_http_cors-python
+
+Reference for caching the `tokenizer` and `model` [3].
+[3]: https://cloud.google.com/functions/docs/bestpractices/tips#do_lazy_initialization_of_global_variables
 """
 
-# `requirements.txt`
-# transformers==4.27.4
-# torch==2.0.0
-# functions-framework==3.3.0
+import transformers
+import functions_framework
+
+def cold_start_tokenier():
+    print('Loading the tokenizer')
+    tokenizer = transformers.AutoTokenizer.from_pretrained('facebook/bart-large-cnn',
+                                                           cache_dir='/tmp/',
+                                                           max_length=1024,
+                                                           truncation=True)
+    print('Loaded the tokenizer')
+    return tokenizer
+
+def cold_start_model():
+    print('Loading the model')
+    model = transformers.AutoModelForSeq2SeqLM.from_pretrained('facebook/bart-large-cnn',
+                                                               cache_dir='/tmp/')
+    print('Loaded the model')
+    return model
+
+# Lazy initialization
+tokenizer, model = None, None
 
 @functions_framework.http
 def main(request):
     # Set CORS headers for the preflight request
     if request.method == 'OPTIONS':
+        # Allows GET requests from any origin with the Content-Type
+        # header and caches preflight response for an 3600s
         headers = {
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': '*',
@@ -36,21 +60,27 @@ def main(request):
         'Access-Control-Allow-Origin': '*'
     }
 
-    # Get the request
     request_json = request.get_json()
     print(request_json)
 
-    # Load the summarizer
-    print('Loading the tokenizer & model')
-    tokenizer = transformers.AutoTokenizer.from_pretrained('facebook/bart-large-cnn', cache_dir='/tmp/', max_length=1024, truncation=True)
-    model = transformers.AutoModelForSeq2SeqLM.from_pretrained('facebook/bart-large-cnn', cache_dir='/tmp/')
-    summarizer = transformers.pipeline('summarization', model=model, tokenizer=tokenizer, truncation=True)
-    print('Loaded the tokenizer & model')
+    # Do lazy initialization of `tokenizer` and `model`
+    global tokenizer, model
+    if not tokenizer:
+        tokenizer = cold_start_tokenier()
+    if not model:
+        model = cold_start_model() 
 
-    # Summarize the article
+    print('Constructing the summarizer')
+    summarizer = transformers.pipeline('summarization',
+                                       model=model,
+                                       tokenizer=tokenizer,
+                                       truncation=True)
+    print('Constructed the summarizer')
+
     print('Summarizing')
     article = request_json['article']
     summary = summarizer(article)[0]['summary_text']
+    print('Summarized')
 
     return (summary, 200, headers)
     
--- frontend/uninsider/src/app/pages/summarization/summarization.component.ts
+++ frontend/uninsider/src/app/pages/summarization/summarization.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-const endpoint = 'https://us-central1-bart-proj.cloudfunctions.net/uninsider-bart-cnn-cors-all'
+const endpoint = 'https://us-central1-bart-proj.cloudfunctions.net/uninsider-bart-cnn-cors-all-global'
 const MIN_CHARS = 50;
 
 // [TODO]
@@ -8,9 +8,10 @@ const MIN_CHARS = 50;
 // In this current version, the first request takes a long time to respond.
 // This is because the server must download the `model` and the `tokenizer`.
 //
-// After the first request, the server will cache the `model` and the `tokenizer` in the `/tmp/` folder.
+// After the first request (cold-start), because we use global lazy-initialization,
+// the server will cache the `model` and the `tokenizer` in the `/tmp/` folder.
 //
-// To solve this problem, we can use `prefetch` function to download the model and the tokenizer
+// To solve this problem, we can use a `prefetch` function to download the model and the tokenizer
 // when our app starts. This will give time for the server to download the required files.
 
 @Component({
@@ -19,7 +20,6 @@ const MIN_CHARS = 50;
   styleUrls: ['./summarization.component.css']
 })
 export class SummarizationComponent {
-
   inputText: string = '';
   summarizedText: string = '';
   isSummarizationReady: boolean = false;
@@ -27,14 +27,36 @@ export class SummarizationComponent {
 
   constructor() {}
 
+  public async initSummarization(): Promise<any> {
+    let response = null;
+    try {
+      response = await fetch(endpoint, {
+        method: 'POST',
+        body: JSON.stringify({article: 'init'}),
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': '*',
+          'Access-Control-Allow-Headers': '*',
+        }
+      });
+    } catch (e) {
+      console.log(e);
+      alert('Could not initialize the summarization model');
+      return;
+    }
+
+    if (response.ok) {
+      console.log('Successfully initialized the summarization model');
+    }
+  }
+
   public async summarize(): Promise<any> {
     // Reset some variables
     this.isSummarizationReady = false;
     this.summarizationLoading = true;
     this.summarizedText = '';
 
-    // [TODO]: Ensure that the summarization ends with a period (.)
-    //         -> This can be done as a post-processing step in this script
     // [TODO]: ...
 
     let response = null;
@@ -83,6 +105,15 @@ export class SummarizationComponent {
     // Successfully summarized the text
     // Convert `ReadableStream` to `string` and update the UI
     var summary = await response.text();
+
+    // Ensure that the summary ends with a period (.)
+    // Find the last index of the period (.)
+    let lastPeriodIndex = summary.lastIndexOf('.');
+
+    // Cut the summary at the last period (.)
+    if (lastPeriodIndex != -1)
+      summary = summary.substring(0, lastPeriodIndex + 1);
+
     this.summarizedText = summary;
     this.isSummarizationReady = true;
     this.summarizationLoading = false;
