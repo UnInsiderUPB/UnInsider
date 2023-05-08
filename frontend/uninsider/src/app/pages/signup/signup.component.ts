89c3ee2aae5d48602f4f19aed3dc096998dfc503
Sabina Dinu <dinu_sabina18@yahoo.com>
2023-05-08 21:25:53 +0300

    Create form-control and mat-errors for email, phone and password

--- frontend/uninsider/src/app/pages/signup/signup.component.ts
+++ frontend/uninsider/src/app/pages/signup/signup.component.ts
@@ -2,6 +2,14 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '../../services/user.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  ValidatorFn,
+  Validators
+} from '@angular/forms';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -10,6 +18,18 @@ import Swal from 'sweetalert2';
   styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent {
+
+  registerForm = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    firstname: new FormControl('', [Validators.required]),
+    lastname: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.pattern('^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]).{8,}$')]),
+    phone: new FormControl('', [Validators.required, Validators.pattern('^07\\d{2}\\s\\d{3}\\s\\d{3}$')]),
+    confirmPassword: new FormControl('', Validators.required)
+  }, { validators: confirmPasswordValidator});
+
+
   constructor(
     private userService: UserService,
     private snack: MatSnackBar,
@@ -17,33 +37,20 @@ export class SignupComponent {
   ) {}
 
   public user = {
-    username: '',
-    password: '',
-    firstName: '',
-    lastName: '',
-    email: '',
-    phone: '',
+    username: this.registerForm.value.username,
+    password: this.registerForm.value.password,
+    firstName: this.registerForm.value.firstname,
+    lastName: this.registerForm.value.lastname,
+    email: this.registerForm.value.email,
+    phone: this.registerForm.value.phone
   };
 
   ngOnInit(): void {}
 
   formSubmit() {
-    console.log(this.user);
-
-    // Username sanity check
-    if (this.user.username == '' || this.user.username == null) {
-      this.snack.open('Username cannot be empty!', 'OK', {
-        duration: 3000,
-      });
-      return;
-    }
-
-    // [TODO]: Sanity checks for other fields
-
-    // [TODO]: Validate
 
     // Add user
-    this.userService.addUser(this.user).subscribe({
+    this.userService.addUser(this.registerForm.value).subscribe({
       next: (data) => {
         console.log(data);
         Swal.fire('Success!', 'User created successfully', 'success').then(
@@ -61,3 +68,10 @@ export class SignupComponent {
     });
   }
 }
+
+export const confirmPasswordValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+  const password = control.get('password');
+  const confirmPassword = control.get('confirmPassword');
+
+  return password && confirmPassword && password.value === confirmPassword.value ? { confirmPassword: true } : null;
+};
