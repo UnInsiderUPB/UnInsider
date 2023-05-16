da7063d4c4756d899e4a2622aa96a9cd786a8aa7
Sabina Dinu <dinu_sabina18@yahoo.com>
2023-05-16 15:38:23 +0300

    Modify password error message

--- frontend/uninsider/src/app/pages/signup/signup.component.ts
+++ frontend/uninsider/src/app/pages/signup/signup.component.ts
@@ -16,9 +16,9 @@ import Swal from 'sweetalert2';
 })
 export class SignupComponent {
 
-  errorPassword = "Password must be a combination of lower-case, upper-case, numbers and at least 9 characters long";
-  emailError = "This field should have an email format"
-  phoneError = "Phone should only contain digits."
+  errorPassword = "Password must contain lower-case, upper-case, numbers, and at least 9 characters!";
+  emailError = "This field must have an email format!"
+  phoneError = "Phone should only contain digits!"
   registerForm = new FormGroup({
     username: new FormControl('', [Validators.required]),
     firstname: new FormControl('', [Validators.required]),
