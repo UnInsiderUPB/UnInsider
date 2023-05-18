d6d0d5642b62083e36d1f769a09ee4296742f95f
Iulian <iulian27_marius@yahoo.com>
2023-05-18 13:03:08 +0300

    send `user` object to backend instead of `registerForm`

--- frontend/uninsider/src/app/pages/signup/signup.component.ts
+++ frontend/uninsider/src/app/pages/signup/signup.component.ts
@@ -16,16 +16,17 @@ import Swal from 'sweetalert2';
 })
 export class SignupComponent {
 
-  errorPassword = "Password must contain lower-case, upper-case, numbers, and at least 9 characters!";
-  emailError = "This field must have an email format!"
-  phoneError = "Phone should only contain digits!"
+  errorPassword = "Password must contain lower-case, upper-case, numbers, and at least 9 characters!"
+  emailError    = "This field must have an email format!"
+  phoneError    = "Phone should only contain digits!"
+  
   registerForm = new FormGroup({
-    username: new FormControl('', [Validators.required]),
-    firstname: new FormControl('', [Validators.required]),
-    lastname: new FormControl('', [Validators.required]),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required, Validators.pattern('^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]).{8,}$')]),
-    phone: new FormControl('', [Validators.required, Validators.pattern('^[0-9]+$')]),
+    username:       new FormControl('', [Validators.required]),
+    firstname:      new FormControl('', [Validators.required]),
+    lastname:       new FormControl('', [Validators.required]),
+    email:          new FormControl('', [Validators.required, Validators.email]),
+    password:       new FormControl('', [Validators.required, Validators.pattern('^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]).{8,}$')]),
+    phone:          new FormControl('', [Validators.required, Validators.pattern('^[0-9]+$')]),
   });
 
   constructor(
@@ -35,20 +36,19 @@ export class SignupComponent {
   ) {}
 
   public user = {
-    username: this.registerForm.value.username,
-    password: this.registerForm.value.password,
-    firstName: this.registerForm.value.firstname,
-    lastName: this.registerForm.value.lastname,
-    email: this.registerForm.value.email,
-    phone: this.registerForm.value.phone
+    username:   this.registerForm.value.username,
+    password:   this.registerForm.value.password,
+    firstName:  this.registerForm.value.firstname,
+    lastName:   this.registerForm.value.lastname,
+    email:      this.registerForm.value.email,
+    phone:      this.registerForm.value.phone
   };
 
   ngOnInit(): void {
   }
 
   formSubmit() {
-
-    //every user should have an email, password and username
+    // Check if `username` is empty
     if (this.user.username == '' || this.user.username == null) {
       this.snack.open('Username cannot be empty!', 'OK', {
         duration: 3000,
@@ -56,6 +56,7 @@ export class SignupComponent {
       return;
     }
 
+    // Check if `email` is empty
     if (this.user.email == '' || this.user.email == null) {
       this.snack.open('Email cannot be empty!', 'OK', {
         duration: 3000,
@@ -63,6 +64,7 @@ export class SignupComponent {
       return;
     }
 
+    // Check if `password` is empty
     if (this.user.password == '' || this.user.password == null) {
       this.snack.open('Password cannot be empty!', 'OK', {
         duration: 3000,
@@ -70,6 +72,7 @@ export class SignupComponent {
       return;
     }
 
+    // Validate the register form
     if (!this.registerForm.valid) {
       this.snack.open('Please complete the fields correctly!', 'OK', {
         duration: 3000,
@@ -77,8 +80,8 @@ export class SignupComponent {
       return;
     }
 
-    // Add user
-    this.userService.addUser(this.registerForm.value).subscribe({
+    // Register the user
+    this.userService.addUser(this.user).subscribe({
       next: (data) => {
         console.log(data);
         Swal.fire('Success!', 'User created successfully', 'success').then(
