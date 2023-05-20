f69bce20bbc0ae72b79635747e204aba217763da
Iulian <iulian27_marius@yahoo.com>
2023-05-20 14:34:10 +0300

    solved routing from every page when clicking on navbar

--- frontend/uninsider/src/app/app-routing.module.ts
+++ frontend/uninsider/src/app/app-routing.module.ts
@@ -112,7 +112,11 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {useHash: true})],
+  imports: [RouterModule.forRoot(routes, {
+    useHash: true,
+    anchorScrolling: 'enabled',
+    scrollPositionRestoration: 'enabled'
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
